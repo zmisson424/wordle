2dd19c153d90512488a8693aea92a8c0501e0080
Zach <zmisson424@gmail.com>
2022-07-26 15:34:33 -0500

    retrieve random word

--- lib/api/dictionary.dart
+++ lib/api/dictionary.dart
@@ -1,8 +1,20 @@
+import 'dart:convert';
 import 'dart:developer' as developer;
+import 'package:http/http.dart' as http;
 
 Future<String?> getRandomWord() async {
   try{
-    return 'check';
+
+    http.Response response = await http.get(
+      Uri.parse('https://random-word-api.herokuapp.com/word?length=5')
+    );
+    if(response.statusCode != 200 || response.body.isEmpty){
+      throw Exception('Word not found');
+    }
+
+    List<dynamic> results = json.decode(response.body);
+
+    return results[0] as String;
   }
   catch(e){
     developer.log(
