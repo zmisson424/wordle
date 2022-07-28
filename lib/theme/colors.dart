50cd5bff847f86bc741d70fc958ed3126c6926a3
Zach <zmisson424@gmail.com>
2022-07-28 10:24:01 -0500

    defining variable colors for board to improve consistency

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,5 +1,14 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppColors {
   static const Color primary = Color(0xFF03A9F4);
+
+  static const Color correctLight = Color(0xFF4CAF50);
+  static const Color correctDark = Color(0xFF4CAF50);
+  static const Color closeLight = Color(0xFFFFEE58);
+  static const Color closeDark = Color(0xFFFFEE58);
+  static const Color missLight = Color(0xFF607D8B);
+  static const Color missDark = Color(0xFF607D8B);
 }
