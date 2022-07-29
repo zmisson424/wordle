c367e257ab3740e4a5b5660431b853fb030d84d9
Zach <zmisson424@gmail.com>
2022-07-29 11:09:37 -0500

    added graph

--- lib/views/home/widgets/scoreboard/stats.dart
+++ lib/views/home/widgets/scoreboard/stats.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class Stats extends StatelessWidget {
@@ -10,27 +11,86 @@ class Stats extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-          Row(
-            children: const [
-              _StatBlock(
-                title: 'Games Won', 
-                stat: '12'
-              ),
-              _StatBlock(
-                title: 'Games Played', 
-                stat: '14'
-              ),
-              _StatBlock(
-                title: 'Current Streak', 
-                stat: '2'
-              ),
-              _StatBlock(
-                title: 'Best Streak', 
-                stat: '4'
-              ),
-            ],
-          )
+        Row(
+          children: const [
+            _StatBlock(
+              title: 'Games Won', 
+              stat: '12'
+            ),
+            _StatBlock(
+              title: 'Games Played', 
+              stat: '14'
+            ),
+            _StatBlock(
+              title: 'Current Streak', 
+              stat: '2'
+            ),
+            _StatBlock(
+              title: 'Best Streak', 
+              stat: '4'
+            ),
+          ],
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 300,
+          child: BarChart(
+            generateData(),
+          ),
+        )
       ],
+    );
+  }
+
+  BarChartData generateData(){
+    return BarChartData(
+      barGroups: [
+        BarChartGroupData(
+          x: 1,
+          barRods: [
+            BarChartRodData(
+              fromY: 0,
+              toY: 1
+            )
+          ]
+        ),
+        BarChartGroupData(
+          x: 2,
+          barRods: [
+            BarChartRodData(
+              fromY: 0,
+              toY: 2
+            )
+          ]
+        ),
+        BarChartGroupData(
+          x: 3,
+          barRods: [
+            BarChartRodData(
+              fromY: 0,
+              toY: 5
+            )
+          ]
+        ),
+        BarChartGroupData(
+          x: 4,
+          barRods: [
+            BarChartRodData(
+              fromY: 0,
+              toY: 8
+            )
+          ]
+        ),
+        BarChartGroupData(
+          x: 5,
+          barRods: [
+            BarChartRodData(
+              fromY: 0,
+              toY: 6
+            )
+          ]
+        ),
+      ]
     );
   }
 }
