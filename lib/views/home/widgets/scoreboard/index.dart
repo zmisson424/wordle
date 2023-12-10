32c123828fd57c871d944af829f191ec5be57b2c
Zach <zmisson424@gmail.com>
2023-12-10 16:23:11 -0600

    updated packages

--- lib/views/home/widgets/scoreboard/index.dart
+++ lib/views/home/widgets/scoreboard/index.dart
@@ -1,31 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:wordle/theme/colors.dart';
 import 'package:wordle/views/home/widgets/scoreboard/stats.dart';
 import 'package:wordle/views/home/widgets/scoreboard/times.dart';
 
 class ScoreboardSheet extends StatefulWidget {
-
-  const ScoreboardSheet({
-    Key? key
-  }) : super(key: key);
+  const ScoreboardSheet({Key? key}) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
-  _ScoreboardSheetState createState()=> _ScoreboardSheetState();
-
+  _ScoreboardSheetState createState() => _ScoreboardSheetState();
 }
 
-class _ScoreboardSheetState extends State<ScoreboardSheet> with SingleTickerProviderStateMixin {
-
+class _ScoreboardSheetState extends State<ScoreboardSheet>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
   void initState() {
-    _tabController = TabController(
-      length: 2,
-      vsync: this
-    );
+    _tabController = TabController(length: 2, vsync: this);
     super.initState();
   }
 
@@ -36,52 +28,34 @@ class _ScoreboardSheetState extends State<ScoreboardSheet> with SingleTickerProv
       height: 500,
       child: Column(
         mainAxisSize: MainAxisSize.max,
-        children:  [
+        children: [
           Padding(
-            padding: const EdgeInsets.only(
-              top: 16,
-              bottom: 8
-            ),
-            child: Text(
-              AppLocalizations.of(context)!.scoreboard,
-              style: const TextStyle(
-                fontSize: 24
-              ),
-            )
-          ),
+              padding: const EdgeInsets.only(top: 16, bottom: 8),
+              child: Text(
+                AppLocalizations.of(context)!.scoreboard,
+                style: const TextStyle(fontSize: 24),
+              )),
           Container(
             decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: Colors.blueGrey.shade200,
-                  width: 0.5
-                )
-              )
-            ),
-            child: TabBar(
-              controller: _tabController,
-              tabs: [
-                Tab(
-                  text: AppLocalizations.of(context)!.topTime,
-                ),
-                Tab(
-                  text: AppLocalizations.of(context)!.stats,
-                ),
-              ]
-            ),
+                border: Border(
+                    bottom: BorderSide(
+                        color: Colors.blueGrey.shade200, width: 0.5))),
+            child: TabBar(controller: _tabController, tabs: [
+              Tab(
+                text: AppLocalizations.of(context)!.topTime,
+              ),
+              Tab(
+                text: AppLocalizations.of(context)!.stats,
+              ),
+            ]),
           ),
           Expanded(
             child: TabBarView(
-              controller: _tabController,
-              children: const [
-                TopTimes(),
-                Stats()
-              ]
-            ),
+                controller: _tabController,
+                children: const [TopTimes(), Stats()]),
           )
         ],
       ),
     );
   }
-
 }
